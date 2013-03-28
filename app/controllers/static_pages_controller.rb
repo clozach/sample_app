ad23478bb1658fc2e9b0cd93c0c44912b08f4f22
Chris Lozac'h <clozach@955dreams.com>
2013-03-28 11:23:55 -0700

    3.2.2 - adding 'about'

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,8 @@ class StaticPagesController < ApplicationController
   end
 
   def help
-  	# Rails runs the code here (which is nada), and, absent specific rendering instructions, renders the eponymous view
-    # Note: the view is at <controller_name>/<action_name>, i.e., .../sample_app/static_pages/help
+  end
+
+  def about
   end
 end
