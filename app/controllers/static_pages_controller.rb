c0632351c7ffc4ee77483e355a91908b67060ce9
Chris Lozac'h <clozach@955dreams.com>
2013-03-27 18:32:41 -0700

    minor

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+  	# Rails runs the code here (which is nada), and, absent specific rendering instructions, renders the eponymous view
+    # Note: the view is at <controller_name>/<action_name>, i.e., .../sample_app/static_pages/home
   end
 
   def help
+  	# Rails runs the code here (which is nada), and, absent specific rendering instructions, renders the eponymous view
+    # Note: the view is at <controller_name>/<action_name>, i.e., .../sample_app/static_pages/help
   end
 end
