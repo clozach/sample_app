3e1134cc12330f131395cabc09b984ba00d2f578
Chris Lozac'h <clozach@955dreams.com>
2013-03-29 16:58:07 -0700

    - fixed nested 'describe's - factored out "Sample App"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,33 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { "Sample App" }
+
+  context "Title tag"
   %w[Home Help About Contact].each do |file|
-    it "should contain #{file} in the title" do
-      lower = file.downcase
+    it "should contain #{:base_title} and #{file}" do
+      lower = file.downcase 
       visit "/static_pages/#{lower}" #visit => Capybara method loads content into 'page'
       p file
-      page.should have_selector('title', text: "Sample App | #{file}")
+      page.should have_selector('title', text: "#{base_title} | #{file}")
     end
   end
 
-  describe "Home Page" do
-    it "should have the content 'Sample App'" do
+  context "Home Page" do
+    it "should have the content #{:base_title}" do
     	visit '/static_pages/home' #visit => Capybara method loads content into 'page'
-    	page.should have_selector('h1', text: 'Sample App')
+    	page.should have_selector('h1', text: base_title)
     end
   end
 
-  describe "Help Page" do
+  context "Help Page" do
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
       page.should have_selector('h1', text: 'Help')
   	end
   end
 
-  describe "About page" do
+  context "About page" do
     it "should have the content 'About this unnecessary page'" do
       visit '/static_pages/about'
       page.should have_selector('h1', text: 'About this unnecessary page')
