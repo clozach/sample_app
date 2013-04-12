96d8fe32b7c923a2819b625b2abe9c74f80c233a
Chris Lozac'h <clozach@955dreams.com>
2013-04-11 23:19:57 -0700

    Spec helper for full_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,40 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  let(:base_title) { "Sample App" }
 
-  context "Title tag"
-  %w[Help About Contact].each do |file|
-    it "should contain base_title and #{file}" do
-      lower = file.downcase 
-      path = "#{lower}_path"
-      visit send(path) #visit => Capybara method loads content into 'page'
-      page.should have_selector('title', text: "#{base_title} | #{file}")
-    end
-  end
-
-  # Set subject of remaining tests
   subject { page }
 
-  context "Home Page" do
-    before { visit root_path } #visit => Capybara method loads content into 'page'
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
-    it { should have_selector('h1', text: base_title) }
-    it { should_not have_selector('title', text: "Home") }
+  describe "Home Page" do
+    before { visit root_path } #visit => Capybara method loads content into 'page'
+    let(:page_title) { '' }
+    let(:heading) { 'Sample App' }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: "|") }
   end
 
-  context "Help Page" do
+  describe "Help Page" do
     before { visit help_path }
+    let(:page_title) { 'Help' }
+    let(:heading) { 'Help' }
+    it_should_behave_like "all static pages"
     it { should have_selector('h1', text: 'Help') }
   end
 
-  context "About page" do
-    before {visit about_path }
-    it { should have_selector('h1', text: 'About this unnecessary page') }
+  describe "About page" do
+    before { visit about_path }
+    let(:page_title) { 'About' }
+    let(:heading) { 'About this unnecessary page' }
+    it_should_behave_like "all static pages"
   end
 
-  context "Contact page" do
+  describe "Contact page" do
     before { visit contact_path }
-    it { should have_selector('h1', text: 'Do not contact me, please') }
+    let(:page_title) { 'Contact' }
+    let(:heading) { 'Do not contact me, please' }
   end
 end
