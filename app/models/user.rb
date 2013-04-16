6a10e3efb57430bd1eae942008a468886c5dd26d
Chris Lozac'h <clozach@955dreams.com>
2013-04-15 18:05:13 -0700

    Validation of email uniqueness, v1 (basic validation)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+	attr_accessible :email, :name
 
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
 end
